2c69d4e997adf1b26732642fa418cd6bab082fb1
Omkar <113718239+Omkar-omi@users.noreply.github.com>
2023-02-26 09:46:37 +0530

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,8 @@ import NavBar from "./NavBar";
 import ProductFilter from "./ProductFilter";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import { auth } from "../firebase";
+
 
 const Home = () => {
   const navigate = useNavigate();
