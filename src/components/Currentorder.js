97e47db3f81415fd9b2c6d77bafbe86439bf1a92
Omkar <113718239+Omkar-omi@users.noreply.github.com>
2023-02-26 09:05:33 +0530

    update

--- src/components/Currentorder.js
+++ src/components/Currentorder.js
@@ -1,142 +1,200 @@
-import { collection, deleteField, doc, increment, onSnapshot, query, updateDoc, where } from 'firebase/firestore';
-import { useEffect, useState } from 'react';
-import coffee from '../images/Coffee.jpg'
-import db from '../firebase'
-import { getAuth } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
+import {
+  collection,
+  deleteField,
+  doc,
+  increment,
+  onSnapshot,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+import coffee from "../images/Coffee.jpg";
+import db from "../firebase";
+import { getAuth } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const CurrentOrder = () => {
   const auth = getAuth();
   const user = auth.currentUser;
   const navigate = useNavigate();
-  const [isPending, setIsPending] = useState(false)
-
+  const [isPending, setIsPending] = useState(false);
 
   useEffect(() => {
-    getData()
-
-  }, [])
+    getData();
+  }, []);
 
   const [data, setData] = useState();
   const getData = async () => {
-    const docRef = query(collection(db, "users"), where("name", "==", localStorage.getItem("name")));
+    const docRef = query(
+      collection(db, "users"),
+      where("name", "==", localStorage.getItem("name"))
+    );
     onSnapshot(docRef, (snapshot) => {
-      setData(snapshot.docs.map(doc => ({
-        ...doc.data(),
-        id: doc.id
-      })))
-    })
-  }
+      setData(
+        snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }))
+      );
+    });
+  };
 
   const handelDelete = async (e) => {
     const docRef = doc(db, "users", user.uid);
-    const srno = e.target.value
-    const price = e.target.getAttribute("data-price")
+    const srno = e.target.value;
+    const price = e.target.getAttribute("data-price");
 
     await updateDoc(docRef, {
       [`cart.${srno}`]: deleteField(),
-      cartvalue: increment(-price)
-    })
-  }
+      cartvalue: increment(-price),
+    });
+  };
   const handelCheckout = () => {
-    setIsPending(true)
+    setIsPending(true);
     setTimeout(() => {
-      navigate("/checkout")
-      setIsPending(false)
-    }, 500)
-  }
+      navigate("/checkout");
+      setIsPending(false);
+    }, 500);
+  };
 
-  if (data && Object.keys(data[0].cart).length == 0) {
+  if (data && Object.keys(data[0]?.cart).length == 0) {
     return (
-      <div className="sm:block hidden bg-neutral p-10 w-96 mt-10">
-        <div className="text-2xl text-gray-400 font-semibold mb-7" >Current Order</div>
+      <div className="sm:block hidden bg-neutral p-10 min-w-[384px] mt-10">
+        <div className="text-2xl text-gray-400 font-semibold mb-7">
+          Current Order
+        </div>
         <div className="flex flex-col ">
-          <div className="overflow-y-scroll scroll-m-4 snap-start h-96 border-solid border-b-2 border-primary scrollbar" >{/* outer div */}
+          <div className="overflow-y-scroll scroll-m-4 snap-start h-96 border-solid border-b-2 border-primary scrollbar">
+            {/* outer div */}
             <div>{`Empty Cart : (`}</div>
           </div>
           <div className="mt-11">
             <div className="flex flex-row justify-between border-solid border-b-2 border-primary pb-5">
-              <div className=''>
+              <div className="">
                 <div>Subtotal:</div>
                 <div>CGST 9%:</div>
                 <div>SGST 9%:</div>
               </div>
               <hr />
-              <div className=''>
+              <div className="">
                 <div>₹ 0</div>
                 <div>₹ 0</div>
                 <div>₹ 0</div>
               </div>
             </div>
             <div className="flex flex-row justify-between my-5">
-              <div className='font-medium text-xl'>Total:</div>
+              <div className="font-medium text-xl">Total:</div>
               <div>₹ 0</div>
             </div>
             <div className="flex">
-              <button className="btn btn-primary grow" disabled>Check Out</button>
+              <button className="btn btn-primary grow" disabled>
+                Check Out
+              </button>
             </div>
           </div>
         </div>
-      </div >
-    )
-  }
-  else {
+      </div>
+    );
+  } else {
     return (
-      <div className="sm:block hidden bg-neutral p-10 w-96 mt-10 ">
-        <div className="text-2xl text-gray-400 font-semibold mb-7" >Current Order</div>
+      <div className="sm:block hidden bg-neutral p-10 min-w-[384px] mt-10 ">
+        <div className="text-2xl text-gray-400 font-semibold mb-7">
+          Current Order
+        </div>
         <div className="flex flex-col ">
-          <div className="overflow-y-scroll scroll-m-4 snap-start h-96 border-solid border-b-2 border-primary scrollbar" >{/* outer div */}
-            {data ? data.length > 0 ? Object.values(data[0].cart).map((product) =>
-              <div className="flex flex-row mb-5" key={product.srno}>
-                < img src={coffee} className="h-20 w-20 rounded-xl" alt="coffee" />
-                <div className="mx-4 flex flex-col grow">
-                  <div className='flex items-baseline justify-between'>
-                    <div className='font-medium text-white'>{product.product}</div>
-                    <button onClick={handelDelete} value={product.srno} data-price={product.price} >X</button>
-                  </div>
-                  <div className="flex flex-row  mt-5">
-                    <div className='text-warning mr-14'>{`${product.quantity} X`}</div>
-                    <div>{`₹ ${product.price}`}</div>
-                  </div>
-                </div>
-              </div>
-            ) : null : null}
+          <div className="overflow-y-scroll scroll-m-4 snap-start h-96 border-solid border-b-2 border-primary scrollbar">
+            {/* outer div */}
+            {data
+              ? data.length > 0
+                ? Object.values(data[0].cart).map((product) => (
+                    <div className="flex flex-row mb-5" key={product.srno}>
+                      <img
+                        src={coffee}
+                        className="h-20 w-20 rounded-xl"
+                        alt="coffee"
+                      />
+                      <div className="mx-4 flex flex-col grow">
+                        <div className="flex items-baseline justify-between">
+                          <div className="font-medium text-white">
+                            {product.product}
+                          </div>
+                          <button
+                            onClick={handelDelete}
+                            value={product.srno}
+                            data-price={product.price}
+                          >
+                            X
+                          </button>
+                        </div>
+                        <div className="flex flex-row  mt-5">
+                          <div className="text-warning mr-14">{`${product.quantity} X`}</div>
+                          <div>{`₹ ${product.price}`}</div>
+                        </div>
+                      </div>
+                    </div>
+                  ))
+                : null
+              : null}
           </div>
           <div className="mt-11">
             <div className="flex flex-row justify-between border-solid border-b-2 border-primary pb-5">
-              <div className=''>
+              <div className="">
                 <div>Subtotal:</div>
                 <div>CGST 9%:</div>
                 <div>SGST 9%:</div>
               </div>
               <hr />
               <div>
-                {data ? data.map(product =>
-                  <div key={0}>
-                    <div  >{`₹ ${product.cartvalue}.0`}</div>
-                    <div>₹ {Number((product.cartvalue / 100) * 9).toFixed(2)}</div>
-                    <div>₹ {Number((product.cartvalue / 100) * 9).toFixed(2)}</div>
-                  </div>
-                ) : null
-                }
+                {data
+                  ? data.map((product) => (
+                      <div key={0}>
+                        <div>{`₹ ${product.cartvalue}.0`}</div>
+                        <div>
+                          ₹ {Number((product.cartvalue / 100) * 9).toFixed(2)}
+                        </div>
+                        <div>
+                          ₹ {Number((product.cartvalue / 100) * 9).toFixed(2)}
+                        </div>
+                      </div>
+                    ))
+                  : null}
               </div>
             </div>
             <div className="flex flex-row justify-between my-5">
-              <div className='font-medium text-xl'>Total:</div>
-              {data ? data.map(product =>
-                <div key={1}>₹ {Number(product.cartvalue + (product.cartvalue / 100) * 18).toFixed(2)}</div>
-              ) : null
-              }
+              <div className="font-medium text-xl">Total:</div>
+              {data
+                ? data.map((product) => (
+                    <div key={1}>
+                      ₹{" "}
+                      {Number(
+                        product.cartvalue + (product.cartvalue / 100) * 18
+                      ).toFixed(2)}
+                    </div>
+                  ))
+                : null}
             </div>
             <div className="flex">
-              {isPending && <button className="content-center btn btn-primary grow loading">Checking Out...</button>}
-              {!isPending && <button className="content-center btn btn-primary grow" onClick={handelCheckout}> Check Out</button>}
+              {isPending && (
+                <button className="content-center btn btn-primary grow loading">
+                  Checking Out...
+                </button>
+              )}
+              {!isPending && (
+                <button
+                  className="content-center btn btn-primary grow"
+                  onClick={handelCheckout}
+                >
+                  {" "}
+                  Check Out
+                </button>
+              )}
             </div>
           </div>
         </div>
-      </div >
+      </div>
     );
   }
-}
+};
 
 export default CurrentOrder;
--- src/components/ProductFilter.js
+++ src/components/ProductFilter.js
@@ -1,10 +1,17 @@
 import { getAuth } from "firebase/auth";
-import { collection, doc, getDocs, increment, onSnapshot, setDoc, updateDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDocs,
+  increment,
+  onSnapshot,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
-import db from '../firebase'
-import coffee from '../images/Coffee.jpg'
+import db from "../firebase";
+import coffee from "../images/Coffee.jpg";
 import { AiFillHeart } from "react-icons/ai";
-
 
 const ProductFilter = () => {
   const [data, setData] = useState();
@@ -17,173 +24,282 @@ const ProductFilter = () => {
   const [favouritePrice, setfavouritePrice] = useState();
 
   useEffect(() => {
-    getAllData()
-  }, [])
+    getAllData();
+  }, []);
 
   const getAllData = async () => {
-    const alldata = []
+    const alldata = [];
 
     const querySnapshotcoffee = await getDocs(collection(db, "coffee"));
     querySnapshotcoffee.forEach((doc) => {
-      alldata.push({ ...doc.data(), id: doc.id })
+      alldata.push({ ...doc.data(), id: doc.id });
     });
 
     const querySnapshotdeserts = await getDocs(collection(db, "deserts"));
     querySnapshotdeserts.forEach((doc) => {
-      alldata.push({ ...doc.data(), id: doc.id })
+      alldata.push({ ...doc.data(), id: doc.id });
     });
 
     const querySnapshotsnacks = await getDocs(collection(db, "snacks"));
     querySnapshotsnacks.forEach((doc) => {
-      alldata.push({ ...doc.data(), id: doc.id })
+      alldata.push({ ...doc.data(), id: doc.id });
     });
 
-    setData(alldata)
-    setDataCopy(alldata)
-  }
+    setData(alldata);
+    setDataCopy(alldata);
+  };
 
   const getCoffeType = async (type) => {
     onSnapshot(collection(db, type), (snapshot) => {
-      setData(snapshot.docs.map(doc => ({
-        ...doc.data(),
-        id: doc.id
-      })))
-    })
-  }
+      setData(
+        snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }))
+      );
+    });
+  };
 
-  const truncate = (input) => input?.length > 75 ? `${input.substring(0, 75)}...` : input;
+  const truncate = (input) =>
+    input?.length > 75 ? `${input.substring(0, 75)}...` : input;
 
   const handelAddToCart = async (e) => {
     const auth = getAuth();
     const user = auth.currentUser;
     if (qty > 0) {
-      const docRef = doc(db, "users", localStorage.getItem("id"))
-      setDoc(docRef, {
-        cart: {
-          [`${no}`]: {
-            srno: no,
-            quantity: qty,
-            product: e.target.value,
-            price: price * qty
+      const docRef = doc(db, "users", localStorage.getItem("id"));
+      setDoc(
+        docRef,
+        {
+          cart: {
+            [`${no}`]: {
+              srno: no,
+              quantity: qty,
+              product: e.target.value,
+              price: price * qty,
+            },
           },
-        }
-      }, { merge: true })
+        },
+        { merge: true }
+      );
       updateDoc(docRef, {
         cartvalue: increment(qty * price),
-      })
-      setQty(0)
-      setNo(no + 1)
-      document.querySelectorAll("#input").forEach(input => {
-        input.value = ""
-      })
+      });
+      setQty(0);
+      setNo(no + 1);
+      document.querySelectorAll("#input").forEach((input) => {
+        input.value = "";
+      });
     }
-  }
+  };
 
   const handelFavourite = async () => {
     console.log(index);
-    const docRef = doc(db, "users", localStorage.getItem("id"))
-    setDoc(docRef, {
-      favourite: {
-        [index]: {
-          srno: index,
-          product: favouriteName,
-          price: favouritePrice
-        }
-      }
-    }, { merge: true })
-    setNo(no + 1)
-  }
+    const docRef = doc(db, "users", localStorage.getItem("id"));
+    setDoc(
+      docRef,
+      {
+        favourite: {
+          [index]: {
+            srno: index,
+            product: favouriteName,
+            price: favouritePrice,
+          },
+        },
+      },
+      { merge: true }
+    );
+    setNo(no + 1);
+  };
   if (data == "") {
     return (
       <>
         <div className="flex flex-col lg:flex-row mx-3 mt-20 justify-between">
           <div className="flex flex-row form-control md:grow">
-            <input type="text" placeholder="Search categoty or menu" className="input text-white input-bordered border-primary grow" onChange={(e) => {
-
-              if ((e.target.value).length > 0) {
-                setData(data.filter(product => product.name.toLowerCase().includes((e.target.value).toLowerCase())))
-              } else {
-                setData(dataCopy)
-              }
-            }} />
+            <input
+              type="text"
+              placeholder="Search categoty or menu"
+              className="input text-white input-bordered border-primary grow"
+              onChange={(e) => {
+                if (e.target.value.length > 0) {
+                  setData(
+                    data.filter((product) =>
+                      product.name
+                        .toLowerCase()
+                        .includes(e.target.value.toLowerCase())
+                    )
+                  );
+                } else {
+                  setData(dataCopy);
+                }
+              }}
+            />
           </div>
           <div className="tabs tabs-boxed lg:ml-5 lg:mt-0  mt-5 h-14 bg-neutral items-center rounded-2xl">
-            <div className="tab text-xl text-white hover:text-primary active:text-warning" onClick={getAllData}>All</div>
-            <div className="tab text-xl text-white hover:text-primary active:text-primary" onClick={() => {
-              getCoffeType("coffee")
-            }}>Coffee</div>
-            <div className="tab text-xl text-white hover:text-primary active:text-primary" onClick={() => {
-              getCoffeType("deserts")
-            }}>Deserts</div>
-            <div className="tab text-xl text-white hover:text-primary active:text-primary" onClick={() => {
-              getCoffeType("snacks")
-            }}>Snacks</div>
+            <div
+              className="tab text-xl text-white hover:text-primary active:text-warning"
+              onClick={getAllData}
+            >
+              All
+            </div>
+            <div
+              className="tab text-xl text-white hover:text-primary active:text-primary"
+              onClick={() => {
+                getCoffeType("coffee");
+              }}
+            >
+              Coffee
+            </div>
+            <div
+              className="tab text-xl text-white hover:text-primary active:text-primary"
+              onClick={() => {
+                getCoffeType("deserts");
+              }}
+            >
+              Deserts
+            </div>
+            <div
+              className="tab text-xl text-white hover:text-primary active:text-primary"
+              onClick={() => {
+                getCoffeType("snacks");
+              }}
+            >
+              Snacks
+            </div>
           </div>
         </div>
         <div className="grid place-items-center h-screen text-primary">
           <div className="text-5xl ">No results found</div>
         </div>
       </>
-    )
-  }
-  else {
+    );
+  } else {
     return (
       <>
         <div className="flex flex-col lg:flex-row mx-3 mt-20 justify-between">
           <div className="flex flex-row form-control md:grow">
-            <input type="text" placeholder="Search categoty or menu" className="input text-white input-bordered border-primary grow" onChange={(e) => {
-              if ((e.target.value).length > 0) {
-                setData(data.filter(product => product.name.toLowerCase().includes((e.target.value).toLowerCase())))
-              } else {
-                setData(dataCopy)
-              }
-            }} />
+            <input
+              type="text"
+              placeholder="Search categoty or menu"
+              className="input text-white input-bordered border-primary grow"
+              onChange={(e) => {
+                if (e.target.value.length > 0) {
+                  setData(
+                    data.filter((product) =>
+                      product.name
+                        .toLowerCase()
+                        .includes(e.target.value.toLowerCase())
+                    )
+                  );
+                } else {
+                  setData(dataCopy);
+                }
+              }}
+            />
           </div>
           <div className="tabs tabs-boxed lg:ml-5 lg:mt-0  mt-5 h-14 bg-neutral items-center rounded-2xl">
-            <div className="tab text-xl text-white hover:text-primary active:text-warning" onClick={getAllData}>All</div>
-            <div className="tab text-xl text-white hover:text-primary active:text-primary" onClick={() => {
-              getCoffeType("coffee")
-            }}>Coffee</div>
-            <div className="tab text-xl text-white hover:text-primary active:text-primary" onClick={() => {
-              getCoffeType("deserts")
-            }}>Deserts</div>
-            <div className="tab text-xl text-white hover:text-primary active:text-primary" onClick={() => {
-              getCoffeType("snacks")
-            }}>Snacks</div>
+            <div
+              className="tab text-xl text-white hover:text-primary active:text-warning"
+              onClick={getAllData}
+            >
+              All
+            </div>
+            <div
+              className="tab text-xl text-white hover:text-primary active:text-primary"
+              onClick={() => {
+                getCoffeType("coffee");
+              }}
+            >
+              Coffee
+            </div>
+            <div
+              className="tab text-xl text-white hover:text-primary active:text-primary"
+              onClick={() => {
+                getCoffeType("deserts");
+              }}
+            >
+              Deserts
+            </div>
+            <div
+              className="tab text-xl text-white hover:text-primary active:text-primary"
+              onClick={() => {
+                getCoffeType("snacks");
+              }}
+            >
+              Snacks
+            </div>
           </div>
         </div>
-        <div className="grid 2xl:grid-cols-6 lg:grid-cols-2 xl:grid-cols-3 grid-cols-1 mt-10 justify-items-center">
-          {data ? data.map((product, index) =>
-            <div className="relative card w-80 lg:w-80 bg-neutral shadow-xl mb-5 mx-10" key={product.id}>
-              <div className="absolute top-2 right-2 tooltip tooltip-left rounded-lg" data-tip="Add to Favourite" onMouseEnter={() => {
-                setfavouriteName(product.name)
-                setfavouritePrice(product.price)
-                setIndex(product.srno)
-              }} onTouchStart={() => {
-                setfavouriteName(product.name)
-                setfavouritePrice(product.price)
-                setIndex(product.srno)
-              }}>
-                <AiFillHeart className="btn btn-circle bg-transparent border-0 text-red-600 hover:bg-transparent hover:scale-125" onClick={handelFavourite} />
-              </div>
-              <figure><img src={coffee} alt="coffee" className="w-80 h-52" /></figure>
-              <div className="card-body">
-                <h2 className="card-title text-white h-10 ">{product.name}</h2>
-                <div className="">Description: {truncate(product.description)}</div>
-                <div className="card-actions flex">
-                  <div className="text-green-600 text-lg">Price: ₹ {product.price}</div>
-                  <input type="number" min={0} max={100} id="input" placeholder="Enter Quantity" className=" grow rounded-lg pl-3 py-1 icon" required onChange={(e) => {
-                    setQty(e.target.value)
-                    setPrice(product.price)
-                  }} autoComplete="off" />
+        <div className="flex flex-wrap  mt-10 justify-items-center">
+          {data
+            ? data.map((product, index) => (
+                <div
+                  className="relative card w-80 lg:w-80 bg-neutral shadow-xl mb-5 mx-10"
+                  key={product.id}
+                >
+                  <div
+                    className="absolute top-2 right-2 tooltip tooltip-left rounded-lg"
+                    data-tip="Add to Favourite"
+                    onMouseEnter={() => {
+                      setfavouriteName(product.name);
+                      setfavouritePrice(product.price);
+                      setIndex(product.srno);
+                    }}
+                    onTouchStart={() => {
+                      setfavouriteName(product.name);
+                      setfavouritePrice(product.price);
+                      setIndex(product.srno);
+                    }}
+                  >
+                    <AiFillHeart
+                      className="btn btn-circle bg-transparent border-0 text-red-600 hover:bg-transparent hover:scale-125"
+                      onClick={handelFavourite}
+                    />
+                  </div>
+                  <figure>
+                    <img src={coffee} alt="coffee" className="w-80 h-52" />
+                  </figure>
+                  <div className="card-body">
+                    <h2 className="card-title text-white h-10 ">
+                      {product.name}
+                    </h2>
+                    <div className="">
+                      Description: {truncate(product.description)}
+                    </div>
+                    <div className="card-actions flex">
+                      <div className="text-green-600 text-lg">
+                        Price: ₹ {product.price}
+                      </div>
+                      <input
+                        type="number"
+                        min={0}
+                        max={100}
+                        id="input"
+                        placeholder="Enter Quantity"
+                        className=" grow rounded-lg pl-3 py-1 icon"
+                        required
+                        onChange={(e) => {
+                          setQty(e.target.value);
+                          setPrice(product.price);
+                        }}
+                        autoComplete="off"
+                      />
+                    </div>
+                    <button
+                      className="btn btn-primary"
+                      value={product.name}
+                      onClick={handelAddToCart}
+                    >
+                      Add to cart
+                    </button>
+                  </div>
                 </div>
-                <button className="btn btn-primary" value={product.name} onClick={handelAddToCart}>Add to cart</button>
-              </div>
-            </div>) : null}
-        </div >
+              ))
+            : null}
+        </div>
       </>
     );
   }
-}
+};
 
 export default ProductFilter;
