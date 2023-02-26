e91e2f20c807d84423571159aa81c3035d88e809
Omkar <113718239+Omkar-omi@users.noreply.github.com>
2023-02-26 09:45:06 +0530

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,16 @@
 import CurrentOrder from "./Currentorder";
-import NavBar from "./NavBar"
+import NavBar from "./NavBar";
 import ProductFilter from "./ProductFilter";
-import {  useNavigate } from "react-router-dom";
-import { useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 const Home = () => {
   const navigate = useNavigate();
+  const user = auth.currentUser;
 
-  useEffect(()=>{
-    if(!users) navigate("/login");
-  },[])
+  useEffect(() => {
+    if (!user) navigate("/login");
+  }, []);
 
   return (
     <div>
@@ -22,6 +23,6 @@ const Home = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Home;
